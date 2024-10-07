0d6d360d1484c9967d8bc1f0308b9a5d76731a40
dwl <1275878391@qq.com>
2024-10-07 23:21:32 +0800

    third commit

---
+++ project/form.py
@@ -0,0 +1,11 @@
+from django import forms
+from .models import *
+
+class ProjectForm(forms.ModelForm):
+    name = forms.CharField(max_length=100)
+    description = forms.CharField(max_length=100)
+    register = forms.CharField(min_length=2)
+    project_end = forms.DateField()
+    project_num = forms.IntegerField()
+    project_goal = forms.CharField(min_length=2, max_length=200)
+    project_need = forms.CharField(min_length=2, max_length=200)
--- project/models.py
+++ project/models.py
@@ -5,8 +5,16 @@ from register.models import Register
 class Project(models.Model):
     name = models.CharField(max_length=100,verbose_name='项目名称')
     description = models.CharField(max_length=100, blank=True, null=True, default='暂无',verbose_name='项目描述')
-    register = models.ForeignKey(Register, on_delete=models.CASCADE,verbose_name='项目负责人')
+    register = models.CharField(max_length=100, verbose_name='项目负责人')
+    project_end = models.DateField(blank=True, null=True, verbose_name='项目截止日期')
     project_num = models.SmallIntegerField(default=0,verbose_name='项目人数')
     project_goal = models.CharField(max_length=100, blank=True, null=True, default='暂无',verbose_name='项目目标')
     project_need = models.CharField(max_length=100, blank=True, null=True, default='暂无',verbose_name='项目需求')
+
+
+    def __str__(self):
+        return self.name
+
+    def is_valid(self):
+        pass
 
